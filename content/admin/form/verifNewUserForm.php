c4c27f7a889c7584b970a52e33b92e06cd4640b4
robin-gonseau <robin-gonseau@niwee.fr>
2021-07-20 23:39:11 +0200

    add new path for loginForm, add href fot social network btn and create error var to verifNewUser

--- content/admin/form/verifNewUserForm.php
+++ content/admin/form/verifNewUserForm.php
@@ -1,13 +1,15 @@
 <?php
 if( isset($_POST['userName']) && isset($_POST['password']) && isset($_POST['confirmPassword']) ){
     if(empty($_POST['userName']) || empty($_POST['password']) || empty($_POST['confirmPassword'])){
-        echo "Veuillez remplir tous les champs";
+        $error= "Veuillez remplir tous les champs";
+        require 'newUserForm';
     }else {
         require_once '../../sql/Sql.php';
         $sql = new Sql;
         $user = $sql->getAdmin(htmlspecialchars( $_POST['userName']));
         if(!empty($user)) {
-            echo "Cet utilisateur existe déja ";
+            $error= "Cet utilisateur existe déja ";
+            require 'newUserForm.php';
         }else{   
             if( $_POST['password'] == $_POST['confirmPassword']){                
                 $sql->newAdmin(htmlspecialchars($_POST['userName']),password_hash(htmlspecialchars($_POST['password']), PASSWORD_BCRYPT ));
--- content/template/footer.php
+++ content/template/footer.php
@@ -6,22 +6,22 @@
     <!-- Section: Social media -->
     <section class="mb-4">
       <!-- Facebook -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-light btn-floating m-1" href="https://fr-fr.facebook.com/" target="_blank" role="button"
         ><i class="fab fa-facebook-f"></i>
       </a>
 
       <!-- Twitter -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/?lang=fr" target="_blank" role="button"
         ><i class="fab fa-twitter"></i  >
       </a>     
 
       <!-- Instagram -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/?hl=fr" target="_blank" role="button"
         ><i class="fab fa-instagram"></i> 
       </a>
 
       <!-- Linkedin -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      <a class="btn btn-outline-light btn-floating m-1" href="https://fr.linkedin.com/" target="_blank" role="button"
         ><i class="fab fa-linkedin-in"></i>
       </a>
 
@@ -33,7 +33,7 @@
 
   <!-- Copyright -->
   <div class="text-center p-3 bg-dark" style="background-color:#1F2D41;">
-    © 2021 Copyright : Barber-Home | <a href="../admin/formConnexion/form.php">connexion</a>
+    © 2021 Copyright : Barber-Home | <a href="../admin/form/loginForm.php">connexion</a>
   </div>
 
 </footer>
